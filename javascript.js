1c3e46d0301b48d0658cffe54b771ffe132fed06
pagan-bear <33454013+pagan-bear@users.noreply.github.com>
2018-02-24 11:45:26 +0100

    Random game board
    Added random game board generation
    Changed game board to 8x8
    Added images for grass (good) / sand (bad)
    Added supporting documentation (UML UC diagram/textual)

--- javascript.js
+++ javascript.js
@@ -11,4 +11,34 @@ function randomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-console.log(randomNumber(0, 9));
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+
+// Define the tile images
+var grass = new Image();
+var sand = new Image();
+
+grass.src = 'grassTile.jpg';
+sand.src = 'sandTile.png';
+
+// Initilise the boardArray ()
+var boardArray = [];
+
+// Initialise layout start position - TRH corner for boardArray
+let posX = 0;
+let posY = 0;
+
+for (var i=0; i<8; i++) {
+  // Initialise inner array
+  boardArray[i] = [];
+  for (var j=0; j<8; j++) {
+    boardArray[i][j] = randomNumber(0, 1);
+
+    if (boardArray[i][j] === 0) { ctx.drawImage(grass, posX, posY, 50, 50); }
+    if (boardArray[i][j] === 1) { ctx.drawImage(sand, posX, posY, 50, 50); }
+    posX += 50;
+  }
+    posX = 0;
+    posY += 50;
+}
+console.table(boardArray);
